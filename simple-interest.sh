a822c9e282c9309aa6b3ffea6a77189140f13587
Ray-1964 <fahdkhaliddelhi@gmail.com>
2025-08-02 19:30:52 -0400

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,10 @@
+echo "Welcome to the Simple Interest Calculator!"
 
+read -p "Enter the Principal amount: " principal
+read -p "Enter the Rate of interest: " rate
+read -p "Enter the Time (in years): " time
+
+interest=$(echo "scale=2; ($principal * $rate * $time) / 100" | bc)
+
+echo "The Simple Interest is: $interest"
+Let me know when you're ready to commit and push this file t
